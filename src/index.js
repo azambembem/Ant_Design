d1c8776865225a6f358be0a296d0d1e4c52e7a18
azambembem <azamjan980713@gmail.com>
2024-05-14 23:54:33 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Button2 from "./AntDesign/Button2";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Button2 />);
